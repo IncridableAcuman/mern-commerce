1dde2ea7a723d89c3c2b0e9503643de9543a1c4b
Izzatbek <abdusharipovizzat03@gmail.com>
2025-09-18 22:01:12 +0500

    auth

--- frontend/src/Private/PrivateRoutes.jsx
+++ frontend/src/Private/PrivateRoutes.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 
+// eslint-disable-next-line react/prop-types
 const PrivateRoutes = ({element}) => {
     const token = localStorage.getItem('accessToken');
   return token ? element : <Navigate to={"/auth"} />
